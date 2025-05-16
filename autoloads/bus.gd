3fba6bea8d55a633a1ea0abef05eba393ef223bd
TheGabmeister <thegabmeister@protonmail.com>
2025-05-16 22:47:32 +0800

    Update

--- autoloads/bus.gd
+++ autoloads/bus.gd
@@ -6,5 +6,13 @@ extends Node
 @warning_ignore_start("unused_signal") 
 
 signal enemy_killed(score: int)
+signal player_killed()
+signal player_spawned()
 signal update_score(score: int)
 signal update_hi_score(score: int)
+
+signal sfx_play_sound(sound: AudioStream)
+
+signal music_play(sound: AudioStream)
+signal music_pause_toggle()
+signal music_stop()
